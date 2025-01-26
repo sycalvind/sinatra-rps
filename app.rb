5595f2ea648f1d0dfc6bdf35ab473ab237d09270
sycalvind <114632354+sycalvind@users.noreply.github.com>
2025-01-26 21:21:29 +0000

    completed

--- app.rb
+++ app.rb
@@ -2,15 +2,53 @@ require "sinatra"
 require "sinatra/reloader"
 
 =begin
-1) Create home routine (rules) - copy past html from Target
+1) Create home routine (rules) - copy past html from Target - done!
 2) Create Play Rock, Play Paper, Play Scissors routines
 3) Create View for Play Rock, Play Paper, Play Scissors
 4) layout: add "Play Rock", "Play Paper", "Play Scissors" links
 =end
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+moves = ["rock", "paper", "scissors"]
+
+get("/rock") do
+  @player_move = "rock"
+  @cpu_move = moves.sample
+  if @cpu_move == "rock"
+    @outcome = "tied"
+  elsif @cpu_move == "paper"
+    @outcome = "lost"
+  else 
+    @outcome = "won"
+  end
+  erb(:game)
+end
+
+get("/paper") do
+  @player_move = "paper"
+  @cpu_move = moves.sample
+  if @cpu_move == "rock"
+    @outcome = "won"
+  elsif @cpu_move == "paper"
+    @outcome = "tied"
+  else 
+    @outcome = "lost"
+  end
+  erb(:game)
+end
+
+get("/scissors") do
+  @player_move = "scissors"
+  @cpu_move = moves.sample
+  if @cpu_move == "rock"
+    @outcome = "lost"
+  elsif @cpu_move == "paper"
+    @outcome = "won"
+  else 
+    @outcome = "tied"
+  end
+  erb(:game)
 end
