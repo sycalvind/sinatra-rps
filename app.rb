c9d0355b1d971e10d23fd055392c53dc287e4039
sycalvind <114632354+sycalvind@users.noreply.github.com>
2025-01-26 03:45:26 +0000

    planning

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require "sinatra"
 require "sinatra/reloader"
 
+=begin
+1) Create home routine (rules) - copy past html from Target
+2) Create Play Rock, Play Paper, Play Scissors routines
+3) Create View for Play Rock, Play Paper, Play Scissors
+4) layout: add "Play Rock", "Play Paper", "Play Scissors" links
+=end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
